7be75842d62e8cdd79dba5fd3a4e4ef6630ec421
Keren <sarayguzman09@gmail.com>
2023-06-09 22:15:57 -0500

    final

--- script.js
+++ script.js
@@ -7,6 +7,76 @@ var cambiosRealizados = [];
 boton.addEventListener("click", encriptar);
 
 function encriptar() {
+    var texto = textArea.value;
+    var textoT = "";
+    if (texto === texto.toLowerCase()) {
+        for (var i = 0; i < texto.length; i++) {
+            var elemento = texto[i];
+            if (elemento == "a") {
+                textoT += "ai";
+                console.log(textoT);
+            } else if (elemento == "e") {
+                textoT += "enter";
+            } else if (elemento == "i") {
+                textoT += "imes";
+            } else if (elemento == "o") {
+                textoT += "ober";
+            } else if (elemento == "u") {
+                textoT += "ufat";
+            } else {
+                textoT += texto[i];
+            }
+        }
+    } else {
+        textoT = "solo minusculas";
+    }
+
+    textArea2.value = textoT; // Mostrar todos los cambios en el área de texto
+
+}
+
+boton2.addEventListener("click", desencriptar);
+
+function desencriptar() {
+    var textoe = textArea2.value;
+    var textoT = "";
+    var i = 0;
+
+    if (textoe === textoe.toLowerCase()) {
+        while (i < textoe.length) {
+            var elemento = textoe[i];
+            if (elemento === "a" && textoe.substr(i, 2) === "ai") {
+                textoT += "a";
+                i += 2;
+            } else if (elemento === "e" && textoe.substr(i, 5) === "enter") {
+                textoT += "e";
+                i += 5;
+            } else if (elemento === "i" && textoe.substr(i, 4) === "imes") {
+                textoT += "i";
+                i += 4;
+            } else if (elemento === "o" && textoe.substr(i, 4) === "ober") {
+                textoT += "o";
+                i += 4;
+            } else if (elemento === "u" && textoe.substr(i, 4) === "ufat") {
+                textoT += "u";
+                i += 4;
+            } else {
+                textoT += textoe[i];
+                i++;
+            }
+        }
+    } else {
+        textoT = "solo minusculas";
+    }
+    console.log(textoT);
+    textArea.value = textoT;
+    // Aquí puedes hacer lo que necesites con el texto desencriptado (resultado)
+}
+
+/*
+
+
+function encriptar() {
     var vocales = {
         "a": "1",
         "e": "2",
@@ -14,7 +84,7 @@ function encriptar() {
         "o": "4",
         "u": "5"
     };
-    /*textArea2.value = textArea.value;*/
+    /*textArea2.value = textArea.value;
 
     var encriptador = textArea.value;
     for (var a = 0; a < encriptador.length; a++) {
@@ -59,4 +129,4 @@ function desencriptar() {
         }
     }
     textArea.value = desencriptar;
-}
+}*/
